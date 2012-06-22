40c76a8a29c2b4a1a85015150d1378c2c4abef35
Mathieu Hendey <m@mathieuhendey.com>
2012-06-22 08:19:20 +0100

    Added Yahoo weather parser, condition and temperature label.

--- RedditWeather/FirstViewController.h
+++ RedditWeather/FirstViewController.h
@@ -13,12 +13,19 @@
     CLGeocoder *_geocoder; //Reverse geocoder, gives us information from coordinates.
     
     __weak UILabel *_townLabel;
+    __weak UILabel *_textLabel;
+    __weak UILabel *_temperatureLabel;
 }
 
 @property (strong, nonatomic) CLLocationManager *locationManager; // The location manager code.
 @property (strong, nonatomic) CLLocation *currentLocation; // The location data.
+@property (nonatomic) float longitude;
+@property (nonatomic) float latitude;
 @property (weak, nonatomic) IBOutlet UILabel *townLabel;
+@property (weak, nonatomic) IBOutlet UILabel *textLabel;
+@property (weak, nonatomic) IBOutlet UILabel *temperatureLabel;
 
 - (IBAction) getCoordinates:(id)sender;  //Method we want to be called when update button is tapped.
+- (IBAction)getWeather:(id)sender; //Method we want to be called when get weather button is tapped.
 
 @end
--- RedditWeather/FirstViewController.m
+++ RedditWeather/FirstViewController.m
@@ -5,7 +5,7 @@
 //  Created by Mathieu Hendey on 22/06/2012.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
-
+#define kBgQueue dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
 #import "FirstViewController.h"
 
 @interface FirstViewController ()
@@ -13,7 +13,7 @@
 @end
 
 @implementation FirstViewController
-@synthesize locationManager, currentLocation, townLabel;
+@synthesize locationManager, currentLocation, townLabel, longitude, latitude, textLabel, temperatureLabel;
 
 - (void)viewDidLoad
 {
@@ -44,12 +44,28 @@
     [locationManager startUpdatingLocation];
 }
 
+- (void) getWeather:(id)sender {
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+    
+    // URL for dataWithContentsOfURL.
+    NSString *urlString = [NSString stringWithFormat:@"http://where.yahooapis.com/geocode?location=%f+%f&flags=J&gflags=r&appid=83Q_GxzV34GmB4fZFHAlkt1NOa6YN3.BJ2iyaYv.LeW8uWaUVc0jLJYEISsQdUUVIhoHGw--", latitude, longitude];
+    
+    // Get weather data in background queue so UI doesn't lock up.
+    dispatch_async(kBgQueue, ^{
+    NSData *data = [NSData dataWithContentsOfURL: [NSURL URLWithString:urlString]];
+    [self performSelectorOnMainThread:@selector(fetchedData:)
+                           withObject:data waitUntilDone:YES];
+    });
+    
+}
+
 - (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation {
     // Called when new location data, such as better accuracy for a location or if the device moves.
     
     [locationManager stopUpdatingLocation];
     self.currentLocation = newLocation;
-    
+    latitude = currentLocation.coordinate.latitude;
+    longitude = currentLocation.coordinate.longitude;
     
     CLGeocoder * geoCoder = [[CLGeocoder alloc] init];
     [geoCoder reverseGeocodeLocation:newLocation completionHandler:^(NSArray *placemarks, NSError *error) {
@@ -58,6 +74,7 @@
             townLabel.text = townString;  // Sets townLabel's text to the current town.
         }    
     }];
+    
 }
 
 - (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
@@ -77,4 +94,31 @@
         [alert show];
     }
 }
+
+- (void) fetchedData:(NSData *)responseData {    
+    
+    NSError *error;
+    NSDictionary* json = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:&error];
+    NSDictionary* resultset = [json objectForKey:@"ResultSet"];
+    NSArray* results = [resultset objectForKey:@"Results"];
+    NSDictionary* rawPlaceData = [results objectAtIndex:0];
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    
+    // URL for dataWithContentsOfURL.
+    NSString * urlString = [NSString stringWithFormat: @"http://weather.yahooapis.com/forecastjson?u=c&w=%@", [rawPlaceData objectForKey:@"woeid"]];
+    
+    NSData *data = [NSData dataWithContentsOfURL: [NSURL URLWithString:urlString]];
+    [self performSelectorOnMainThread:@selector(fetchedWeather:)
+                           withObject:data waitUntilDone:YES];
+}
+
+- (void) fetchedWeather:(NSData *)responseData {
+    NSError *error;
+    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:&error];
+    NSDictionary *conditions = [json objectForKey:@"condition"];
+    textLabel.text = [conditions objectForKey:@"text"];
+    temperatureLabel.text = [NSString stringWithFormat: @"%iºC", [[conditions objectForKey:@"temperature"] intValue]];
+    
+}
+
 @end
